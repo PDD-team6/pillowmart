2dac0569954fe0b0500604d4f56e36489defb9a9
Lokesh J <lokeshjreddy2403@gmail.com>
2024-07-02 11:22:41 +0530

    after testing

--- connect.js
+++ connect.js
@@ -2,6 +2,6 @@ import { createPool } from "mysql2";
 export const connect =createPool({
     host:'localhost',
     user:'root',
-    password:'hellow@234',
+    password:'Loki@234',
     database:'pillowmart'
 }).promise();
