1123acbf0d6c3184e1689251793652071fc38d6a
Lokesh J <lokeshjreddy2403@gmail.com>
2024-07-02 18:16:27 +0530

    GIT-2 database name

--- connect.js
+++ connect.js
@@ -2,6 +2,6 @@ import { createPool } from "mysql2";
 export const connect =createPool({
     host:'localhost',
     user:'root',
-    password:'pillowmart@234',
+    password:'pillowparadise@234',
     database:'pillowmart'
 }).promise();
